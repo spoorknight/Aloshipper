9bb0eecfceab41c92f372903c3052b50d6d93b79
Nhật Tiến <spoorknight@gmail.com>
2024-02-03 16:50:06 +0700

    update

---
+++ build/app/generated/source/buildConfig/release/com/aloit/shipper/app_shipper/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aloit.shipper.app_shipper;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.aloit.shipper.app_shipper";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 40;
+  public static final String VERSION_NAME = "1.0.39";
+}
